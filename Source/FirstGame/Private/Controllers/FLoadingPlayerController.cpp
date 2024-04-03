09a3e812aa1e0774c03baf633ed3375327355aca
20191645 <anfki2354@naver.com>
2024-04-03 15:17:38 +0900

    fix: 레벨(LoadingStage) Background 수정

--- Source/FirstGame/Private/Controllers/FLoadingPlayerController.cpp
+++ Source/FirstGame/Private/Controllers/FLoadingPlayerController.cpp
@@ -8,9 +8,12 @@ void AFLoadingPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
+	/*
+	// 로딩 화면 딜레이를 위해 해당 코드를 블루프린트로 구현
 	AGameModeBase* GM = UGameplayStatics::GetGameMode(this);
 	if (true == ::IsValid(GM)) {
 		// 다음 레벨로 이동
 		UGameplayStatics::OpenLevel(GM, TEXT("FirstStage"));
 	}
+	*/
 }
